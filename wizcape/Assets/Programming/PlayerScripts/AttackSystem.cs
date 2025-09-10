5c92fa777fec67814e9f3e8b083e5b70e9e08ccb
JustinMakesGames <justinlf75@gmail.com>
2025-09-10 12:47:44 +0200

    Hand animation with hitting

--- wizcape/Assets/Programming/PlayerScripts/AttackSystem.cs
+++ wizcape/Assets/Programming/PlayerScripts/AttackSystem.cs
@@ -4,15 +4,10 @@ using UnityEngine.InputSystem;
 
 public class AttackSystem : MonoBehaviour
 {
-    [SerializeField] private Transform hand;
-    private Animator _handAnimator;
+    [SerializeField] private Animator _handAnimator;
     private bool _isAttacking;
 
     [SerializeField] private DamageInstance handslap;
-    private void Awake()
-    {
-        _handAnimator = hand.GetComponent<Animator>();
-    }
 
     public void AttackInput(InputAction.CallbackContext context)
     {
@@ -26,7 +21,7 @@ public class AttackSystem : MonoBehaviour
     {
         _isAttacking = true;
         _handAnimator.SetTrigger("Attack");
-        if(Physics.Raycast(ray: new Ray(hand.position, hand.forward), out RaycastHit hitInfo, maxDistance: 20f))
+        if(Physics.Raycast(ray: new Ray(transform.position, transform.forward), out RaycastHit hitInfo, maxDistance: 20f))
         {
             if (hitInfo.collider.gameObject.TryGetComponent(out IDamagable damagable))
             {
